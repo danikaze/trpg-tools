360efffa747237c38a0e321c1749eec7c7adf11f
danikaze <danikaze@gmail.com>
2021-01-30 17:24:40 +0900

    Name generator (GET) api

---
+++ api/names/get.ts
@@ -0,0 +1,56 @@
+import { apiError, ApiHandler } from '@api';
+import { callApi } from '@utils/call-api';
+import { generateName, NameType, Race } from '@utils/generate-name';
+import { getUniqueElems } from '@utils/get-unique-elems';
+
+interface Return {
+  data: string[];
+}
+
+interface Query {
+  race: string;
+  type: string;
+}
+
+const SHOW_NAMES = 10;
+
+/*
+ * API Handler
+ */
+export const getNamesApi: ApiHandler<string[], Query> = (req, res) => {
+  try {
+    const race = req.query.race as Race;
+    const type = req.query.type as NameType<Race>;
+
+    const names = getNameList(race, type);
+    return res.json({ data: getUniqueElems(names) });
+  } catch (e) {
+    apiError(res, e);
+    return;
+  }
+};
+
+/*
+ * Function to call the API from the client
+ */
+export const callGenerateNames = <R extends Race>(race: R, type: string) => {
+  return callApi<Return, Query>('names', 'GET', {
+    params: { race, type },
+  });
+};
+
+/*
+ * Function to obtain the same result from the server (without calling the API)
+ */
+export function getNameList<R extends Race>(
+  race: R,
+  type: NameType<R>
+): string[] {
+  const names: string[] = [];
+
+  for (let i = 0; i < SHOW_NAMES; i++) {
+    names.push(generateName(race, type));
+  }
+
+  return getUniqueElems(names);
+}
