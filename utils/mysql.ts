f22f5f69664465099d14163dcf1a6d03285e538f
danikaze <danikaze@gmail.com>
2021-02-16 20:01:35 +0900

    Add MySql.paginate method

--- utils/mysql.ts
+++ utils/mysql.ts
@@ -1,9 +1,9 @@
 import { Connection, ResultSetHeader } from 'mysql2/promise';
 import { NsLogger } from './logger';
 
-export interface SqlParams {
-  [key: string]: string | number;
-}
+export type SqlParams = {
+  [key: string]: string | number | null;
+};
 
 export interface MySqlOptions {
   /** If provided, it will log some */
@@ -22,6 +22,15 @@ export interface TransactionOptions {
   errHandler?: (e: Error) => void;
 }
 
+export interface Paginated<T> {
+  totalResults: number;
+  totalPages: number;
+  page: number;
+  rpp: number;
+  moreResults: boolean;
+  data: T[];
+}
+
 export interface InitDbOptions {
   /** List of updates/migrations to perform when opening the database */
   updates?: { [toVersion: number]: DbInitFunction };
@@ -30,6 +39,25 @@ export interface InitDbOptions {
    * If not provided, it will apply all needed updates
    */
   version?: number;
+}
+
+export interface SqlLimitConfig {
+  default: number;
+  min: number;
+  max: number;
+}
+
+export type SqlLimits<T extends { [query: string]: string }> = Partial<
+  { [query in keyof T]: SqlLimitConfig }
+>;
+
+// This is just to prevent assigning this parameters from outside,
+// because they are received from the paginate method options
+interface PaginationParams {
+  /** Number of Result per Page */
+  rpp?: never;
+  /** First result to show */
+  offset?: never;
 }
 
 interface DbControl {
@@ -77,6 +105,90 @@ export class MySql {
     params?: P
   ): Promise<T | undefined> {
     return ((await this.connection.execute(sql, params))[0] as T[])[0];
+  }
+
+  /**
+   * Executes a query but returns it as paginated data
+   */
+  public async paginate<
+    T extends {} = {},
+    DP extends {} = SqlParams & PaginationParams,
+    CP extends {} = SqlParams
+  >(options: {
+    /** Results per Page */
+    rpp: number;
+    /** Page to retrieve (starting on 0) */
+    page: number;
+    /** Query to retrieve the data */
+    dataSql: string;
+    /**
+     * Parameters for the dataSql
+     * Must include `offset` at least
+     * (`limit` can be hardcoded in the sql)
+     */
+    dataParams: DP;
+    /**
+     * Query to retrieve the total count
+     * Must be SELECT COUNT(*) AS total...
+     */
+    countSql: string;
+    /**
+     * Optional parameters for the countSql
+     */
+    countParams?: CP;
+    /**
+     * Can be specified to protect queries from aggresive queries so not too
+     * much data is retrieved at once
+     */
+    limit: SqlLimitConfig;
+  }): Promise<Paginated<T>> {
+    if (!IS_PRODUCTION) {
+      if (
+        options.dataSql.toLowerCase().indexOf('limit :rpp') === -1 ||
+        options.dataSql.toLowerCase().indexOf('offset :offset') === -1
+      ) {
+        throw new Error(
+          `Pagination data SQL needs to use "OFFSET :offset" and "LIMIT :rpp". Query: ${options.dataSql}`
+        );
+      }
+      if (
+        options.countSql.toLowerCase().indexOf('limit') !== -1 ||
+        options.countSql.toLowerCase().indexOf('offset') !== -1
+      ) {
+        throw new Error(
+          `Pagination SQL can't use OFFSET nor LIMIT. Query: ${options.countSql}`
+        );
+      }
+    }
+    const { rpp, page, limit } = options;
+    (options.dataParams as SqlParams).rpp = limit
+      ? limit.max < rpp
+        ? limit.default
+        : limit.min || 1 > rpp
+        ? limit.default
+        : rpp
+      : rpp;
+    (options.dataParams as SqlParams).offset = Math.max(0, page) * rpp;
+
+    const [dataQuery, countQuery] = await Promise.all([
+      this.connection.execute(options.dataSql, options.dataParams),
+      this.connection.execute(options.countSql, options.countParams),
+    ]);
+    const data = dataQuery[0] as T[];
+    const count = (countQuery[0] as { total: number }[])[0]!;
+
+    const totalResults = count.total;
+    const totalPages = Math.ceil(totalResults / rpp);
+    const moreResults = page < totalPages - 1;
+
+    return {
+      totalResults,
+      totalPages,
+      moreResults,
+      data,
+      rpp: options.rpp,
+      page: options.page,
+    };
   }
 
   /**
