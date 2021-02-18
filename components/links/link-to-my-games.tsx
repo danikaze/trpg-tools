0f2f67f3fe627bd5ade5f26139da4dde737a90f3
danikaze <danikaze@gmail.com>
2021-02-18 22:41:16 +0900

    My Games pagination

--- components/links/link-to-my-games.tsx
+++ components/links/link-to-my-games.tsx
@@ -3,14 +3,18 @@ import { FunctionComponent } from 'react';
 
 export interface Props {
   className?: string;
+  page?: number;
 }
 
 export const LinkToMyGames: FunctionComponent<Props> = ({
   className,
+  page,
   children,
 }) => {
+  const url = page! > 0 ? `/my-games/page/${page! + 1}` : `/my-games`;
+
   return (
-    <Link href="/my-games">
+    <Link href={url}>
       <a className={className}>{children || 'My Games'}</a>
     </Link>
   );
--- pages/my-games/[[...slug]].tsx
+++ pages/my-games/[[...slug]].tsx
@@ -1,4 +1,4 @@
-import { AppPage, GetServerSideProps } from './_app';
+import { AppPage, GetServerSideProps } from '../_app';
 import { PRODUCT_NAME } from '@utils/constants';
 import { Page } from '@components/page';
 import { MyGames, Props } from '@components/pages/my-games';
@@ -24,17 +24,28 @@ MyGamesPage.defaultProps = {
 export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
   userRequired(ctx);
   const {
-    req: { user },
+    req: { user, url },
   } = ctx;
+  const { page } = getParams(url!);
 
   const props: Props = {};
   if (user) {
-    props.games = await selectUserGames(user.id);
+    props.pages = await selectUserGames(user.id, page);
   }
 
   return {
     props,
   };
 };
+
+function getParams(url: string) {
+  // url is something like '/my-games/page/2' in server side
+  // and '/_next/data/development/my-games/page/3.json?slug=page&slug=3'
+  // in client side
+  const match = /page\/(\d+)/.exec(url);
+  return {
+    page: ((match && Number(match[1])) || 1) - 1,
+  };
+}
 
 export default MyGamesPage;
