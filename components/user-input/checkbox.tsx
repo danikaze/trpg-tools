36fbafe756f6bb5ed9986e605a108b4bca835ef0
danikaze <danikaze@gmail.com>
2021-03-09 20:43:47 +0900

    Checkbox UserInput component

---
+++ components/user-input/checkbox.tsx
@@ -0,0 +1,49 @@
+import clsx from 'clsx';
+import { createRef, FunctionComponent, RefObject } from 'react';
+import { makeStyles } from '@utils/styles';
+import { Label } from './label';
+
+export interface Props {
+  inputRef?: RefObject<HTMLInputElement>;
+  defaultValue?: boolean | string;
+  label?: string;
+  required?: boolean;
+  onChange?: (value: boolean) => void;
+  className?: string;
+}
+
+const useStyles = makeStyles(() => ({
+  root: {},
+}));
+
+type Styles = ReturnType<typeof useStyles>;
+
+export const Checkbox: FunctionComponent<Props> = ({
+  className,
+  inputRef,
+  defaultValue,
+  label,
+  required,
+  onChange,
+}) => {
+  const styles = useStyles();
+  const ref = inputRef || createRef<HTMLInputElement>();
+  const changeHandler = onChange && (() => onChange(ref.current!.checked));
+
+  return (
+    <div className={clsx(styles.root, className)}>
+      {label && <Label required={required}>{label}</Label>}
+      <input
+        type="checkbox"
+        ref={ref}
+        className={clsx(styles.root, className)}
+        defaultChecked={isSelected(defaultValue)}
+        onChange={changeHandler}
+      />
+    </div>
+  );
+};
+
+function isSelected(value: boolean | string | undefined): boolean {
+  return value === true || value === 'true';
+}
