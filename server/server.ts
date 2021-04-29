28a09a99137e5673f6ee1c9da03c2cf8bcec85bd
danikaze <danikaze@gmail.com>
2021-04-29 14:08:10 +0900

    Serve static assets directly with Express as well

--- server/server.ts
+++ server/server.ts
@@ -19,6 +19,7 @@ export function run() {
   server.use(compress());
   server.use(cookieParser());
   server.use(express.urlencoded({ extended: false }));
+  server.use(express.static(`${PROJECT_ROOT}/.next/static`));
 
   if (AUTH_ENABLED) {
     useAuth(server);
