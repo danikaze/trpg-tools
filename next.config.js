f3b3a8404231e033a9b4bfa2ceb9ab591483a1ec
danikaze <danikaze@gmail.com>
2021-05-28 20:17:20 +0900

    Enable customization for the source of the build-time constants (#20)

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer');
 const withFiles = require('./build-tools/with-files');
 const withConstants = require('./build-tools/with-constants');
 
-const { BODY_MAX_SIZE_B } = require('./build-time-constants/global');
-
 let config = withConstants();
 config = withFiles({
   ...config,
@@ -22,9 +20,6 @@ config = withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' })(
 
 config.api = {
   ...config.api,
-  bodyParser: {
-    sizeLimit: `${BODY_MAX_SIZE_B}b`,
-  },
 };
 
 module.exports = config;
